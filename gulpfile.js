8e2ce07dd8df4154cb155db30f318ab3a12bb5ad
Joon-Sub Chung <joonsubtalk@gmail.com>
2016-02-21 09:08:42 -0800

    minifier automated

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,21 @@
 
 var gulp = require('gulp');
 	uglify = require('gulp-uglify');
+	rename = require('gulp-rename');
 
-// scripts
+//	scripts
+//	Know thy Globs
+//		more info: https://github.com/isaacs/node-glob
+//	css/*.css 		# matches files ending in .css in the css directory
+//	css/**/*.css 	# matches files ending in .css in the css directory and child directories
+//	!css/style.css	# excludes style.css files
+//	*.+(js|css)		# matches all files in the root directory ending in .js or .css
+	
 gulp.task('scripts', function(){
-	console.log('it works');
+	gulp.src(['public/javascripts/**/*.js', '!app/js/**/*/min.js'])
+	.pipe(rename({suffix:'.min'}))
+	.pipe(uglify())
+	.pipe(gulp.dest('public/javascripts'));
 });
 
 // default task
