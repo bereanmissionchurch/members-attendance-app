fccbd5e5b485fec6f33b9a946d29e97c97d0ac09
Joon-Sub Chung <joonsubtalk@gmail.com>
2016-02-21 08:53:33 -0800

    Setting up Gulp for  automation

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'susy'
+require 'breakpoint'
+
+#compass config
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "fonts"
+javascript_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :compressed
+relative_assets = true
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+// requried
+
+var gulp = require('gulp');
+	uglify = require('gulp-uglify');
+
+// scripts
+gulp.task('scripts', function(){
+	console.log('it works');
+});
