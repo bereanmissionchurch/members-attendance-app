869aa930afd9e3fbbd30155f297659b1e6d31792
nathankwok <nateckwok@gmail.com>
2016-03-14 14:57:49 -0700

    Basic User model

--- app/models/Users.scala
+++ app/models/Users.scala
@@ -1,10 +1,25 @@
 package models
 
-import scala.slick.driver.PostgresDriver.simple._
+import play.api.libs.json._
+
+import slick.driver.PostgresDriver.simple._
+
+//import scala.slick.driver.PostgresDriver.simple._
 import play.api.Play.current
 import play.api.data.Forms._
+
+
 case class User(name: String, id: Option[Int] = None)
+
 class Users(tag: Tag) extends Table[User](tag, "USERS") {
+
+	implicit val locationWrites = new Writes[User] {
+		def writes(user: User) = Json.obj(
+		"lat" -> user.name,
+		"long" -> user.id
+		)
+	}
+
   // Auto Increment the id primary key column
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   // The name can't be null
@@ -18,7 +33,8 @@ object Users {
 	val db = play.api.db.slick.DB
 	val users = TableQuery[Users]
 	def all: List[User] = db.withSession { implicit session =>
-		users.sortBy(_.id.asc.nullsFirst).list
+		users.list
+//		users.sortBy(_.id.asc.nullsFirst).list
 	}
 	def create(newuser: User) = db.withTransaction{ implicit session =>
 		users += newuser
@@ -33,3 +49,12 @@ object Users {
 		users.filter(_.id === id).delete
 	}
 }
+
+//import play.api.libs.json._
+//
+//implicit val locationWrites = new Writes[User] {
+//	def writes(user: User) = Json.obj(
+//	"lat" -> user.name,
+//	"long" -> user.id
+//	)
+//}
